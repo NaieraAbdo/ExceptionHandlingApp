4fa4c36c5db12d616a4bd72c14fb76facff67049
naiera <nairaabdelhamed6@gmail.com>
2025-02-11 15:59:34 +0200

    Add project files.

---
+++ CheckForVowels/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CheckForVowels
+{
+    internal class Program
+    {
+         static bool ContainsVowels(string input)
+    {
+        foreach (char c in input.ToLower())
+        {
+            if ("aeiou".Contains(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+        static void CheckForVowels(string input)
+        {
+            if (!ContainsVowels(input))
+            {
+                throw new InvalidOperationException("The string does not contain any vowels.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string:");
+
+            string input = Console.ReadLine();
+            try
+            {
+                CheckForVowels(input);
+                Console.WriteLine("The string contains vowels.");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ExceptionHandlingApp/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ExceptionHandlingApp
+{
+    internal class Program
+    {
+        static List<int> ReadNumbersFromUser()
+        {
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Enter integers and break if you're done:");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "break")
+                {
+                    break;
+                }
+
+                if (int.TryParse(input, out int number))
+                {
+                    
+                    if (numbers.Contains(number))
+                    {
+                        throw new InvalidOperationException($"Duplicate number detected: {number}");
+                    }
+                    numbers.Add(number);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter an integer.");
+                }
+            }
+
+            return numbers;
+        }
+        static void Main(string[] args)
+         {
+            try
+            {
+                List<int> nums = ReadNumbersFromUser();
+                Console.WriteLine("Numbers:");
+                foreach (var num in nums)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
